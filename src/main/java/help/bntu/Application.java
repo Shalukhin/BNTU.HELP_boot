99aa572f1122ae9b1bbb19116742ff4c1c3fe9c1
Vasiliy <Vasiliy@DESKTOP-VTMHAII>
2021-02-21 23:48:14 +0300

    add spring boot

--- src/main/java/help/bntu/Application.java
+++ src/main/java/help/bntu/Application.java
@@ -1,12 +1,21 @@
 package help.bntu;
 
-public class Application  {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication (exclude = HibernateJpaAutoConfiguration.class)
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		
-		System.out.println("HI");
+		SpringApplication.run(Application.class, args);
 	}
 	
-	
+	@Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	      return builder.sources(Application.class);
+	  }
 
 }
